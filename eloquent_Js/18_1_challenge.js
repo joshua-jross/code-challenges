87dc18079d4fd1e2ac59d9cac7d112c9a758c546
Joshua Ross <jjross.dev@gmail.com>
2020-12-07 23:01:50 -0800

    added 18_1 solution

--- eloquent_Js/18_1_challenge.js
+++ eloquent_Js/18_1_challenge.js
@@ -1,1 +1,15 @@
 // HTTP and Forms -- Content negotiation
+const url = "https://eloquentjavascript.net/author";
+const types = ["text/plain",
+  "text/html",
+  "application/json",
+  "application/rainbows+unicorns"];
+
+async function showTypes() {
+  for (let type of types) {
+    let resp = await fetch(url, { headers: { accept: type } });
+    console.log(`${type}: ${await resp.text()}\n`);
+  }
+}
+
+showTypes();
